5dfea57b715b1d6282a8ed5236f106d392738636
shahinsba <shahinsba@users.noreply.github.com>
2022-05-08 20:53:28 +0000

    Add test functions for each instruction type.

--- test.py
+++ test.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-Test module for angr tricore lifter.
-
+""" Test module for angr tricore lifter.
 Usage: python3 test.py
 """
 
@@ -9,7 +7,6 @@ import sys
 import os
 import unittest
 import binascii
-import struct
 import yaml
 import angr
 from angr_platforms.tricore import LifterTRICORE  # pylint: disable=[unused-import]
@@ -102,6 +99,14 @@ def get_reg_val(state, regname):
 def get_mem_val(state, addr, size):
     return state.solver.eval(state.memory.load(addr, size, endness="Iend_LE"), cast_to=int)
 
+def change_to_LE(val):
+    """ change endianess to LE. """
+    result = ((val         & 0xff) << 24) | \
+             (((val >> 8)  & 0xff) << 16) | \
+             (((val >> 16) & 0xff) << 8)  | \
+             ((val >> 24)  & 0xff)
+    return result
+
 # read test file
 with open(TEST_FILE, "rb") as stream:
     try:
@@ -109,107 +114,236 @@ with open(TEST_FILE, "rb") as stream:
     except yaml.YAMLError as e:
         print(e)
         sys.exit(1)
-
 instructions = data['instructions']
 
 class TestTricoreLifter(unittest.TestCase):
     """ Class for testing tricore lifter.  """
 
-    def test_rc_instructions(self):
-        """ Test instructions with RC format. """
+    def run_tests(self, tests):
+        """ Run tests. """
+        # iterate through test instructions
+        for item in tests:
+            ins_name = item['instruction'][0]['name']
+            ins_asm = item['instruction'][1]['asm']
+            ins_hex = item['instruction'][2]['hex']
 
-        # iterate through instruction formats
-        for instr_frmt in instructions:
+            print("\n", "-"*50,
+                  "\n---- Test cases for '{0}'".format(ins_name),
+                  "\n---- {0}\n".format(ins_asm))
 
-            if instr_frmt != "RC_format":
-                # skip formats except RC
+            # continue if the instruction has no test case
+            if item['instruction'][3]['testcases'] is None:
                 continue
 
-            print("\n", "="*50)
-            print("= {0} instructions".format(instr_frmt))
-            print("="*50)
+            # iterate through testcases
+            testcase_counter = 1
+            for testcase in item['instruction'][3]['testcases']:
+                case_item = 'case{0}'.format(testcase_counter)
+                testcase_counter += 1
+                print("---- {0}: {1}\n".format(case_item, testcase[case_item][2]['desc']))
 
-            # iterate through instructions
-            num_instructions = 0
-            num_testcases = 0
-            for item in instructions[instr_frmt]:
-                num_instructions += 1
-                ins_name = item['instruction'][0]['name']
-                ins_asm = item['instruction'][1]['asm']
-                ins_hex = item['instruction'][2]['hex']
+                p = angr.load_shellcode(binascii.unhexlify(ins_hex),
+                                        'tricore',
+                                        start_offset=0,
+                                        load_address=0)
+                state = p.factory.entry_state()
 
-                print("\n", "-"*50)
-                print("---- Test cases for '{0}'".format(ins_name))
-                print("---- {0}\n".format(ins_asm))
+                # reset registers
+                for reg in ALL_REGS:
+                    setattr(state.regs, reg, 0)
 
-                # continue if there is no test case
-                if item['instruction'][3]['testcases'] is None:
-                    continue
-
-                # iterate through testcases
-                testcase_counter = 1
-                for testcase in item['instruction'][3]['testcases']:
-                    case_item = 'case{0}'.format(testcase_counter)
-                    print("---- {0}: {1}\n".format(case_item, testcase[case_item][2]['desc']))
-
-                    p = angr.load_shellcode(binascii.unhexlify(ins_hex),
-                                            'tricore',
-                                            start_offset=0,
-                                            load_address=0)
-                    state = p.factory.entry_state()
-
-                    # reset registers
-                    for reg in ALL_REGS:
-                        setattr(state.regs, reg, 0)
-
-                    # apply preconditions
-                    if testcase[case_item][0]['preconditions']:
-                        for precondition in testcase[case_item][0]['preconditions']:
-                            for reg, val in precondition.items():
-                                if reg[:1] == "[":
-                                    if len(hex(val)[2:]) > 8:  # for 64-bit values
-                                        adr_1 = int(reg[1:-1], 16)
-                                        adr_2 = adr_1 + 4
-                                        val_1 = val & 0xffffffff
-                                        val_2 = val >> 32
-                                        state.mem[adr_1].uint32_t = struct.unpack("<I", struct.pack(">I", val_1))[0]
-                                        state.mem[adr_2].uint32_t = struct.unpack("<I", struct.pack(">I", val_2))[0]
-                                    else:
-                                        state.mem[int(reg[1:-1], 16)].uint32_t = struct.unpack("<I", struct.pack(">I",
-                                                                                                                val))[0]
+                # apply preconditions
+                if testcase[case_item][0]['preconditions']:
+                    for precondition in testcase[case_item][0]['preconditions']:
+                        for reg, val in precondition.items():
+                            if reg[:1] == "[":
+                                if len(hex(val)[2:]) > 8:  # for 64-bit values
+                                    adr_1 = int(reg[1:-1], 16)
+                                    adr_2 = adr_1 + 4
+                                    val_1 = val & 0xffffffff
+                                    val_2 = val >> 32
+                                    state.mem[adr_1].uint32_t = change_to_LE(val_1)
+                                    state.mem[adr_2].uint32_t = change_to_LE(val_2)
                                 else:
-                                    setattr(state.regs, reg, val)
+                                    state.mem[int(reg[1:-1], 16)].uint32_t = change_to_LE(val)
+                            else:
+                                setattr(state.regs, reg, val)
 
-                    # run simulation
-                    sm = p.factory.simulation_manager(state)
-                    sm.explore(n=1)
+                # run simulation
+                sm = p.factory.simulation_manager(state)
+                sm.explore(n=1)
 
-                    # check result
-                    if len(sm.active) > 0:
-                        if testcase[case_item][1]['result']:
-                            for result in testcase[case_item][1]['result']:
-                                for reg, val in result.items():
-                                    if reg[:1] == "[":
-                                        size = 4
-                                        mem_val = get_mem_val(sm.active[0], int(reg[1:-1], 16),size)
-                                        self.assertEqual(mem_val, val)
-                                    else:
-                                        reg_val = get_reg_val(sm.active[0], reg)
-                                        reg_val &= 0xffffffff  # get 32-bit value
-                                        self.assertEqual(reg_val, val)
-                        else:
-                            print("Result is empty!")
+                # check result
+                if len(sm.active) > 0:
+                    if testcase[case_item][1]['result']:
+                        for result in testcase[case_item][1]['result']:
+                            for reg, val in result.items():
+                                if reg[:1] == "[":
+                                    size = 4
+                                    mem_val = get_mem_val(sm.active[0], int(reg[1:-1], 16), size)
+                                    self.assertEqual(hex(mem_val), hex(val))
+                                else:
+                                    reg_val = get_reg_val(sm.active[0], reg)
+                                    reg_val &= 0xffffffff  # get 32-bit value
+                                    self.assertEqual(hex(reg_val), hex(val))
                     else:
-                        print("No Stash!")
-                        print("Check the following:")
-                        print("  - if instruction '{0}' has been added to lift_tricore module.".format(ins_name))
-                        print("  - if opcode of '{0}' is correct in its class.".format(ins_name))
-                        sys.exit(1)
+                        print("Result is empty!")
+                else:
+                    print("No Stash!")
+                    print("Check the following:")
+                    print("  - if instruction '{0}' has been added to lift_tricore module.".format(ins_name))
+                    print("  - if opcode of '{0}' is correct in its class.".format(ins_name))
+                    sys.exit(1)
 
-                    testcase_counter += 1
-                    num_testcases += 1
+    def test_abs_instructions(self):
+        """ Test instructions with ABS format. """
+        print("\n", "="*20, "Testing {0} ABS format instructions".format(len(instructions['ABS_format'])), "="*20)
+        self.run_tests(instructions['ABS_format'])
 
-            print("{0} testcases passed for {1} {2} instruction(s)".format(num_testcases,num_instructions,instr_frmt))
+    def test_absb_instructions(self):
+        """ Test instructions with ABSB format. """
+        print("\n", "="*20, "Testing {0} ABSB format instructions".format(len(instructions['ABSB_format'])), "="*20)
+        self.run_tests(instructions['ABSB_format'])
+
+    def test_bit_instructions(self):
+        """ Test instructions with BIT format. """
+        print("\n", "="*20, "Testing {0} BIT format instructions".format(len(instructions['BIT_format'])), "="*20)
+        self.run_tests(instructions['BIT_format'])
+
+    def test_bo_instructions(self):
+        """ Test instructions with BO format. """
+        print("\n", "="*20, "Testing {0} BO format instructions".format(len(instructions['BO_format'])), "="*20)
+        self.run_tests(instructions['BO_format'])
+
+    def test_bol_instructions(self):
+        """ Test instructions with BOL format. """
+        print("\n", "="*20, "Testing {0} BOL format instructions".format(len(instructions['BOL_format'])), "="*20)
+        self.run_tests(instructions['BOL_format'])
+
+    def test_rc_instructions(self):
+        """ Test instructions with RC format. """
+        print("\n", "="*20, "Testing {0} RC format instructions".format(len(instructions['RC_format'])), "="*20)
+        self.run_tests(instructions['RC_format'])
+
+    def test_rcpw_instructions(self):
+        """ Test instructions with RCPW format. """
+        print("\n", "="*20, "Testing {0} RCPW format instructions".format(len(instructions['RCPW_format'])), "="*20)
+        self.run_tests(instructions['RCPW_format'])
+
+    def test_rcr_instructions(self):
+        """ Test instructions with RCR format. """
+        print("\n", "="*20, "Testing {0} RCR format instructions".format(len(instructions['RCR_format'])), "="*20)
+        self.run_tests(instructions['RCR_format'])
+
+    def test_rcrr_instructions(self):
+        """ Test instructions with RCRR format. """
+        print("\n", "="*20, "Testing {0} RCRR format instructions".format(len(instructions['RCRR_format'])), "="*20)
+        self.run_tests(instructions['RCRR_format'])
+
+    def test_rlc_instructions(self):
+        """ Test instructions with RLC format. """
+        print("\n", "="*20, "Testing {0} RLC format instructions".format(len(instructions['RLC_format'])), "="*20)
+        self.run_tests(instructions['RLC_format'])
+
+    def test_rr_instructions(self):
+        """ Test instructions with RR format. """
+        print("\n", "="*20, "Testing {0} RR format instructions".format(len(instructions['RR_format'])), "="*20)
+        self.run_tests(instructions['RR_format'])
+
+    def test_rr1_instructions(self):
+        """ Test instructions with RR1 format. """
+        print("\n", "="*20, "Testing {0} RR1 format instructions".format(len(instructions['RR1_format'])), "="*20)
+        self.run_tests(instructions['RR1_format'])
+
+    def test_rr2_instructions(self):
+        """ Test instructions with RR2 format. """
+        print("\n", "="*20, "Testing {0} RR2 format instructions".format(len(instructions['RR2_format'])), "="*20)
+        self.run_tests(instructions['RR2_format'])
+
+    def test_rrpw_instructions(self):
+        """ Test instructions with RRPW format. """
+        print("\n", "="*20, "Testing {0} RRPW format instructions".format(len(instructions['RRPW_format'])), "="*20)
+        self.run_tests(instructions['RRPW_format'])
+
+    def test_rrr_instructions(self):
+        """ Test instructions with RRR format. """
+        print("\n", "="*20, "Testing {0} RRR format instructions".format(len(instructions['RRR_format'])), "="*20)
+        self.run_tests(instructions['RRR_format'])
+
+    def test_rrr1_instructions(self):
+        """ Test instructions with RRR1 format. """
+        print("\n", "="*20, "Testing {0} RRR1 format instructions".format(len(instructions['RRR1_format'])), "="*20)
+        self.run_tests(instructions['RRR1_format'])
+
+    def test_rrr2_instructions(self):
+        """ Test instructions with RRR2 format. """
+        print("\n", "="*20, "Testing {0} RRR2 format instructions".format(len(instructions['RRR2_format'])), "="*20)
+        self.run_tests(instructions['RRR2_format'])
+
+    def test_rrrr_instructions(self):
+        """ Test instructions with RRRR format. """
+        print("\n", "="*20, "Testing {0} RRRR format instructions".format(len(instructions['RRRR_format'])), "="*20)
+        self.run_tests(instructions['RRRR_format'])
+
+    def test_rrrw_instructions(self):
+        """ Test instructions with RRRW format. """
+        print("\n", "="*20, "Testing {0} RRRW format instructions".format(len(instructions['RRRW_format'])), "="*20)
+        self.run_tests(instructions['RRRW_format'])
+
+    def test_sc_instructions(self):
+        """ Test instructions with SC format. """
+        print("\n", "="*20, "Testing {0} SC format instructions".format(len(instructions['SC_format'])), "="*20)
+        self.run_tests(instructions['SC_format'])
+
+    def test_slr_instructions(self):
+        """ Test instructions with SLR format. """
+        print("\n", "="*20, "Testing {0} SLR format instructions".format(len(instructions['SLR_format'])), "="*20)
+        self.run_tests(instructions['SLR_format'])
+
+    def test_slro_instructions(self):
+        """ Test instructions with SLRO format. """
+        print("\n", "="*20, "Testing {0} SLRO format instructions".format(len(instructions['SLRO_format'])), "="*20)
+        self.run_tests(instructions['SLRO_format'])
+
+    def test_sr_instructions(self):
+        """ Test instructions with SR format. """
+        print("\n", "="*20, "Testing {0} SR format instructions".format(len(instructions['SR_format'])), "="*20)
+        self.run_tests(instructions['SR_format'])
+
+    def test_src_instructions(self):
+        """ Test instructions with SRC format. """
+        print("\n", "="*20, "Testing {0} SRC format instructions".format(len(instructions['SRC_format'])), "="*20)
+        self.run_tests(instructions['SRC_format'])
+
+    def test_sro_instructions(self):
+        """ Test instructions with SRO format. """
+        print("\n", "="*20, "Testing {0} SRO format instructions".format(len(instructions['SRO_format'])), "="*20)
+        self.run_tests(instructions['SRO_format'])
+
+    def test_srr_instructions(self):
+        """ Test instructions with SRR format. """
+        print("\n", "="*20, "Testing {0} SRR format instructions".format(len(instructions['SRR_format'])), "="*20)
+        self.run_tests(instructions['SRR_format'])
+
+    def test_srrs_instructions(self):
+        """ Test instructions with SRRS format. """
+        print("\n", "="*20, "Testing {0} SRRS format instructions".format(len(instructions['SRRS_format'])), "="*20)
+        self.run_tests(instructions['SRRS_format'])
+
+    def test_ssr_instructions(self):
+        """ Test instructions with SSR format. """
+        print("\n", "="*20, "Testing {0} SSR format instructions".format(len(instructions['SSR_format'])), "="*20)
+        self.run_tests(instructions['SSR_format'])
+
+    def test_ssro_instructions(self):
+        """ Test instructions with SSRO format. """
+        print("\n", "="*20, "Testing {0} SSRO format instructions".format(len(instructions['SSRO_format'])), "="*20)
+        self.run_tests(instructions['SSRO_format'])
+
+    def test_sys_instructions(self):
+        """ Test instructions with SYS format. """
+        print("\n", "="*20, "Testing {0} SYS format instructions".format(len(instructions['SYS_format'])), "="*20)
+        self.run_tests(instructions['SYS_format'])
 
 if __name__ == '__main__':
     unittest.main()
